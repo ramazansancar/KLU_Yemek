292c93da72d1d3f4a5e9c222e95f0faa56f0374c
Ramazan Sancar <ramazansancar4545@gmail.com>
2022-11-24 11:31:51 +0000

    sh edited.

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 git config --global user.email $mail
 git config --global user.name $name
 git remote set-url origin https://$username:$token@github.com/$username/KLU_Yemek.git
-if git status --porcelain | grep -q 'list.json'; then 
+if git status --porcelain | grep -q 'list*'; then 
 	git add list.json
+	git add list_all.json
+	git add list_raw.json
 	git commit -am 'update list'
 	echo $username | echo $token | git push -f origin HEAD:master
 else
