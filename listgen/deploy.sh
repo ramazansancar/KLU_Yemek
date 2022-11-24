d508a1fdfd3578eee48710771badc29069ab4b3b
Ramazan Sancar <ramazansancar4545@gmail.com>
2022-11-24 08:08:56 +0000

    Username changed.

--- listgen/deploy.sh
+++ listgen/deploy.sh
@@ -1,10 +1,10 @@
 git config --global user.email $mail
 git config --global user.name $name
-git remote set-url origin https://$username:$token@github.com/berkantkz/KLU_Yemek.git
+git remote set-url origin https://$username:$token@github.com/$username/KLU_Yemek.git
 if git status --porcelain | grep -q 'list.json'; then 
 	git add list.json
 	git commit -am 'update list'
-	echo berkantkz | echo $token | git push -f origin HEAD:master	
+	echo $username | echo $token | git push -f origin HEAD:master	
 else
 	echo 'No changes detected'
 fi
